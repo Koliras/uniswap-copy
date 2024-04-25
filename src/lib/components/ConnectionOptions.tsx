a2d2f1a92264c1c380252517056ae5bd3b8e37ca
Koliras <gyrmanchyk230506@gmail.com>
2024-04-25 13:18:35 +0300

    feat: make component for connection options

---
+++ src/lib/components/ConnectionOptions.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import { ConnectionType, getHasMetaMaskExtensionInstalled } from '../connections'
+import { METAMASK_URL } from '../constants'
+import { Option } from './Option'
+
+type ConnectOptionsParams = {
+	activeConnectionType: ConnectionType | null
+	isConnectionActive: boolean
+	onActivate: (connectionType: ConnectionType) => void
+	onDeactivate: (connectionType: null) => void
+}
+
+export const ConnectionOptions = ({
+	activeConnectionType,
+	isConnectionActive,
+	onActivate,
+	onDeactivate,
+}: ConnectOptionsParams) => {
+	const hasMetaMaskExtension = getHasMetaMaskExtensionInstalled()
+
+	const isNoOptionActive = !isConnectionActive || (isConnectionActive && activeConnectionType === null)
+
+	const metaMaskOption = hasMetaMaskExtension ? (
+		<Option
+			isEnabled={isNoOptionActive || activeConnectionType === ConnectionType.INJECTED}
+			isConnected={activeConnectionType === ConnectionType.INJECTED}
+			connectionType={ConnectionType.INJECTED}
+			onActivate={onActivate}
+			onDeactivate={onDeactivate}
+		/>
+	) : (
+		<a href={METAMASK_URL}>
+			<button>Install Metamask</button>
+		</a>
+	)
+
+	const walletConnectOption = (
+		<Option
+			isEnabled={isNoOptionActive || activeConnectionType === ConnectionType.WALLET_CONNECT}
+			isConnected={activeConnectionType === ConnectionType.WALLET_CONNECT}
+			connectionType={ConnectionType.WALLET_CONNECT}
+			onActivate={onActivate}
+			onDeactivate={onDeactivate}
+		/>
+	)
+
+	return (
+		<div>
+			{metaMaskOption}
+			{walletConnectOption}
+		</div>
+	)
+}
+
