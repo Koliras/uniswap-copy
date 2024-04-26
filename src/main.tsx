edf16907a5352c025ebd3efc702ae13254a23222
Koliras <gyrmanchyk230506@gmail.com>
2024-04-26 14:19:47 +0300

    feat: add react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Web3ContextProvider } from './lib/components/Web3ContextProvider.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Web3ContextProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Web3ContextProvider>
   </React.StrictMode>,
 )
