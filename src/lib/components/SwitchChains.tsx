94d58b701d9b66b86edda76229f4bb2809357b22
Koliras <gyrmanchyk230506@gmail.com>
2024-04-30 10:07:50 +0300

    fix: delete rebase traces

--- src/lib/components/SwitchChains.tsx
+++ src/lib/components/SwitchChains.tsx
@@ -8,20 +8,12 @@ type Props = {
 export const SwitchChains = ({ connectionType }: Props) => {
 	return (
 		<div>
-<<<<<<< HEAD
-			{Object.keys(CHAIN_INFO).map((chainId) => (
-=======
 			{Object.entries(CHAIN_INFO).map(([chainId, chainInfo]) => (
->>>>>>> 7018c96 (feat: separate switching chains in component)
 				<button
 					onClick={() => switchNetwork(parseInt(chainId), connectionType)}
 					key={chainId}
 				>
-<<<<<<< HEAD
-					{`Switch to ${CHAIN_INFO[chainId]?.label}`}
-=======
 					Switch to ${chainInfo.label}
->>>>>>> 7018c96 (feat: separate switching chains in component)
 				</button>
 			))}
 		</div>
