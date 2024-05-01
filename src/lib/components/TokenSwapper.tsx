b6e45abacfb79c8826083e24c1fe5a44c9241190
Koliras <gyrmanchyk230506@gmail.com>
2024-05-01 15:06:09 +0300

    feat: get balance

--- src/lib/components/TokenSwapper.tsx
+++ src/lib/components/TokenSwapper.tsx
@@ -1,8 +1,13 @@
 import { TokenChooser } from "./TokenChooser"
+import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json'
 import TextField from '@mui/material/TextField'
 import InputAdornment from '@mui/material/InputAdornment'
 import Button from '@mui/material/Button'
 import { useTokensStore } from "../stores/tokensStore"
+import { useEffect, useState } from "react"
+import { ethers } from 'ethers'
+import { CHAIN_INFO, ChainId } from "../constants"
+import { useWeb3React } from "@web3-react/core"
 
 export const TokenSwapper = () => {
 	const {
@@ -13,8 +18,28 @@ export const TokenSwapper = () => {
 		flipTokens,
 	} = useTokensStore(state => state);
 
+	const [balance, setBalance] = useState<ethers.BigNumber | null>(null)
+	const { account, chainId } = useWeb3React()
+	const provider = new ethers.providers.JsonRpcProvider(CHAIN_INFO[chainId as ChainId]?.rpcUrl || '')
+
+	const contract = new ethers.Contract(
+		'0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6',
+		Quoter.abi,
+		provider
+	)
+
+	useEffect(() => {
+
+		async function getBalance() {
+			const newBalance = await contract.provider.getBalance(account || '')
+			setBalance(newBalance)
+		}
+		if (account) getBalance()
+	}, [chainId, account])
+
 	return (
 		<div>
+			<h2>{balance?.toString() || "Didn't get"}</h2>
 			<Button
 				onClick={flipTokens}
 			>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -29,7 +29,7 @@ export const CHAIN_INFO: Record<ChainId, ChainInfo> = {
     explorer: 'https://etherscan.io/',
     label: 'Ethereum',
     nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-    rpcUrl: 'https://api.mycryptoapi.com/eth',
+    rpcUrl: 'https://1rpc.io/eth',
   },
   [ChainId.polygon]: {
     explorer: 'https://polygonscan.com/',
